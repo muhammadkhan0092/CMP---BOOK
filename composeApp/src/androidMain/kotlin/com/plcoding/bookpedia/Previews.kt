dc85a4f58ec94a34c342b5e32fd6fee0e8b8ad98
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2026-01-03 21:45:10 +0500

    Search bar Composable

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -7,14 +7,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.bookpedia.book.domain.Book
-import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
-import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview
 @Composable
-private fun BookSearchBarPreview() {
+internal fun BookSearchBarPreview() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -22,37 +19,10 @@ private fun BookSearchBarPreview() {
     ) {
         BookSearchBar(
             searchQuery = "",
-            onSearchQueryChange = {},
-            onImeSearch = {},
+            onSearchQueryChanged = {},
+            onImeiSearch = {},
             modifier = Modifier
                 .fillMaxWidth()
         )
     }
-}
-
-private val books = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Book $it",
-        imageUrl = "https://test.com",
-        authors = listOf("Philipp Lackner"),
-        description = "Description $it",
-        languages = emptyList(),
-        firstPublishYear = null,
-        averageRating = 4.67854,
-        ratingCount = 5,
-        numPages = 100,
-        numEditions = 3
-    )
-}
-
-@Preview
-@Composable
-private fun BookListScreenPreview() {
-    BookListScreen(
-        state = BookListState(
-            searchResults = books
-        ),
-        onAction = {}
-    )
 }
