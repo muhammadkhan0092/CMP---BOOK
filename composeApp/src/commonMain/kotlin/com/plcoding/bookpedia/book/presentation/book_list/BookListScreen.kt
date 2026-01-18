432eb2770cbf5ece61e59eca6e9698f129a4d217
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2026-01-18 17:02:43 +0500

    Tabs Ui And Working

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -7,6 +7,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.bookpedia.book.domain.Book
@@ -14,10 +21,17 @@ import org.koin.compose.viewmodel.koinViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.favorites
+import cmp_bookpedia.composeapp.generated.resources.search_results
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 import com.plcoding.bookpedia.core.presentation.DarkBlue
+import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.stringResource
 
 
 @Composable
@@ -62,5 +76,57 @@ private fun BookListScreen(
                 .fillMaxWidth()
                 .padding(60.dp)
         )
+        Surface(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            color = DesertWhite,
+            shape = RoundedCornerShape(topStart = 32.dp,
+                topEnd = 32.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier.padding(vertical = 12.dp)
+                        .widthIn(max = 700.dp)
+                        .fillMaxWidth(),
+                    indicator = {tabPositions->
+                        TabRowDefaults.SecondaryIndicator(
+                            color = SandYellow,
+                            modifier = Modifier.tabIndicatorOffset(tabPositions[state.selectedTabIndex])
+                        )
+                    }
+                ) {
+                    Tab(
+                        selected = state.selectedTabIndex==0,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(0))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(0.5f)
+                    ){
+                        Text(
+                            modifier = Modifier.padding(12.dp),
+                            text = stringResource(Res.string.search_results)
+                        )
+                    }
+                    Tab(
+                        selected = state.selectedTabIndex==1,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(1))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(0.5f)
+                    ){
+                        Text(
+                            modifier = Modifier.padding(12.dp),
+                            text = stringResource(Res.string.favorites)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
