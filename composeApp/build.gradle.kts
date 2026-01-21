fede3018b926e727908db02a0e447a0a2267cd65
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2026-01-21 21:12:34 +0500

    Book dto

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -47,6 +47,7 @@ kotlin {
             implementation(libs.koin.android)
             implementation(libs.koin.androidx.compose)
             implementation(libs.ktor.client.okhttp)
+            implementation("com.lordcodes.turtle:turtle:0.5.0")
         }
         commonMain.dependencies {
             implementation(compose.runtime)
--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.plcoding.bookpedia
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
