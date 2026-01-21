f34f486aef70d0dd72e94053c0375906beffd759
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2026-01-21 21:42:16 +0500

    Error handling helpers

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -0,0 +1,14 @@
+package com.plcoding.bookpedia.book.data.network
+
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.Result
+import io.ktor.client.HttpClient
+
+class KtorRemoteBookDataSource(
+    private val httpClient : HttpClient
+) {
+    suspend fun searchBooks(query : String,resultLimit : Int? = null) : Result<Book, DataError.Remote>{
+
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/domain/Error.kt
@@ -0,0 +1,4 @@
+package com.plcoding.bookpedia.core.domain
+
+interface Error {
+}
