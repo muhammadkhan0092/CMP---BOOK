957a58ed019233fb7f5861fb660ba366aa740f51
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2026-02-11 21:30:36 +0500

    Book Details Ui And Functionality Except Description

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -1,14 +1,42 @@
 package com.plcoding.bookpedia.book.presentation.book_detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.languages
+import cmp_bookpedia.composeapp.generated.resources.pages
+import cmp_bookpedia.composeapp.generated.resources.rating
 import com.plcoding.bookpedia.book.presentation.book_detail.components.BlurredImageBackground
+import com.plcoding.bookpedia.book.presentation.book_detail.components.BookChip
+import com.plcoding.bookpedia.book.presentation.book_detail.components.ChipSize
 import com.plcoding.bookpedia.book.presentation.book_detail.models.BookDetailAction
 import com.plcoding.bookpedia.book.presentation.book_detail.models.BookDetailState
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun BookDetailScreenRoot(
@@ -36,12 +64,79 @@ private fun BookDetailScreen(
     BlurredImageBackground(
         imageUrl = state.book?.imageUrl,
         isFav = state.isFav,
-        onFavClicked = {onAction(BookDetailAction.OnFavClick)},
+        onFavClicked = { onAction(BookDetailAction.OnFavClick) },
         onBackClicked = {
             onAction(BookDetailAction.OnBackClick)
         },
         modifier = Modifier.fillMaxSize()
-    ){
+    ) {
+        if (state.book != null) {
+            Column(
+                modifier = Modifier
+                    .widthIn(max = 700.dp)
+                    .fillMaxSize()
+                    .padding(vertical = 16.dp, horizontal = 24.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Column(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        state.book.title,
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Text(
+                        state.book.authors.joinToString(", "),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RatingAndPages(
+                            stringResource(Res.string.rating),
+                            state.book.averageRating?.toString() ?: "-"
+                        )
+                        RatingAndPages(
+                            stringResource(Res.string.pages),
+                            state.book.numPages?.toString() ?: "-"
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(stringResource(Res.string.languages))
+                    BookChip(modifier = Modifier, size = ChipSize.SMALL) {
+                        Text(state.book.languages.firstOrNull() ?: "-", color = Color.Black)
+                    }
+                }
+            }
+        }
+    }
+}
 
+@Composable
+private fun RatingAndPages(
+    heading: String,
+    contentText: String,
+    isIcon: Boolean = false
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(heading)
+        Row {
+            BookChip(modifier = Modifier, size = ChipSize.REGULAR) {
+                Text(contentText, color = Color.Black)
+            }
+            if (isIcon) {
+                Spacer(modifier = Modifier.width(5.dp))
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = stringResource(Res.string.rating),
+                    tint = Color.Yellow
+                )
+            }
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
@@ -1,26 +1,53 @@
 package com.plcoding.bookpedia.book.presentation.book_detail.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.book_cover
+import cmp_bookpedia.composeapp.generated.resources.book_error_2
+import cmp_bookpedia.composeapp.generated.resources.go_back
+import cmp_bookpedia.composeapp.generated.resources.mark_as_favorite
+import cmp_bookpedia.composeapp.generated.resources.remove_from_favorites
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 
@@ -33,40 +60,146 @@ fun BlurredImageBackground(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
+    println("IMAGE URL IS $imageUrl")
     var imageLoadResult by remember {
         mutableStateOf<Result<Painter>?>(null)
     }
-    rememberAsyncImagePainter(
+    val painter = rememberAsyncImagePainter(
         model = imageUrl,
         onSuccess = {
             val size = it.painter.intrinsicSize
             imageLoadResult = if (size.width > 1 && size.height > 1) {
                 Result.success(it.painter)
             } else Result.failure(Exception("Invalid image dimensions"))
+        },
+        onError = {
+            imageLoadResult = Result.failure(Exception("Other Error"))
         }
     )
     Box() {
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
-            Box(
-                modifier = Modifier
-                    .weight(0.3f)
-                    .fillMaxWidth()
-                    .background(DarkBlue)
-            ) {
-                imageLoadResult?.getOrNull()?.let {painter->
-                    Image(
-                        painter = painter,
-                        contentDescription = stringResource(Res.string.book_cover),
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize().blur(20.dp)
-                    )
-                }
-            }
-            Box(modifier = Modifier.weight(0.7f).fillMaxWidth().background(DesertWhite)){
-
+            BlurredBox(
+                Modifier
+                    .weight(0.3f),
+                imageLoadResult = imageLoadResult
+            )
+            Box(modifier = Modifier.weight(0.7f).fillMaxWidth().background(DesertWhite)) {
             }
         }
+        BackButtonComposable(
+            modifier = Modifier.align(Alignment.TopStart),
+            onBackClicked = onBackClicked
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Spacer(modifier = Modifier.fillMaxHeight(0.15f))
+            ElevatedCard(
+                modifier = Modifier
+                    .width(200.dp)
+                    .aspectRatio(2 / 3f),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = Color.Transparent
+                ),
+                elevation = CardDefaults.elevatedCardElevation(
+                    defaultElevation = 15.dp
+                )
+            ) {
+                AnimatedContent(
+                    targetState = imageLoadResult
+                ) { result ->
+                    when {
+                        result == null -> {
+                            println("IN IF ELSE -> NULL")
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+                        result.isSuccess -> {
+                            println("IN IF ELSE -> ELSE")
+                            Box() {
+                                Image(
+                                    painter = if (result.isSuccess) painter else painterResource(Res.drawable.book_error_2),
+                                    contentDescription = stringResource(Res.string.book_cover),
+                                    modifier = Modifier.fillMaxSize()
+                                        .background(Color.Transparent),
+                                    contentScale = if (result.isSuccess) ContentScale.Crop else ContentScale.Fit
+                                )
+                                FavButtonComposable(
+                                    modifier = Modifier
+                                        .align(Alignment.BottomEnd),
+                                    onFavClicked = onFavClicked,
+                                    isFav = isFav
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            content()
+        }
+    }
+}
+
+@Composable
+fun BlurredBox(modifier: Modifier, imageLoadResult: Result<Painter>?) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(DarkBlue)
+    ) {
+        imageLoadResult?.getOrNull()?.let { painter ->
+            Image(
+                painter = painter,
+                contentDescription = stringResource(Res.string.book_cover),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize().blur(20.dp)
+            )
+        }
+    }
+}
+
+@Composable()
+fun FavButtonComposable(modifier: Modifier, onFavClicked: () -> Unit, isFav: Boolean) {
+    IconButton(
+        onClick = onFavClicked,
+        modifier = modifier
+            .background(
+                brush = Brush.radialGradient(
+                    colors = listOf(SandYellow, Color.Transparent),
+                    radius = 70f
+                )
+            )
+    ) {
+        Icon(
+            imageVector = if (isFav) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+            contentDescription = if (isFav) stringResource(Res.string.remove_from_favorites) else stringResource(
+                Res.string.mark_as_favorite
+            ),
+            tint = Color.Red,
+        )
+    }
+}
+
+@Composable
+fun BackButtonComposable(modifier: Modifier, onBackClicked: () -> Unit) {
+    IconButton(
+        onClick = onBackClicked,
+        modifier = modifier
+            .padding(top = 16.dp, start = 16.dp)
+            .statusBarsPadding(),
+    ) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            contentDescription = stringResource(Res.string.go_back),
+            tint = Color.White
+        )
     }
 }
